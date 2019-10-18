05a67149e0a440872b69953cf86ae73cd7ba45ba
Wagner Souza <wagner.souza@accenture.com>
2019-10-18 13:47:46 -0300

    Adds default route for / Fixes response of xml2Json

--- src/controllers/XmlController.js
+++ src/controllers/XmlController.js
@@ -1,12 +1,13 @@
-const Xml = require('../models/Xml')
+const xml = require('../models/Xml')
 const parser = require('xml-js')
 
 module.exports = {
   index(req, res) {
     res.set('Content-Type', 'text/xml')
-    res.send(Xml)
+    res.send(xml)
   },
   xmlToJson(req, res) {
-    res.send(parser.xml2json(Xml, { compact: true, spaces: 4 }))
+    res.set('content-type', 'application/json')
+    res.send(parser.xml2json(xml, { compact: true, spaces: 4 }))
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,10 @@
+const { resolve } = require('path')
 const express = require('express')
 const XmlController = require('./controllers/XmlController')
 
 const routes = new express.Router()
+
+routes.get('/', (req, res) => res.sendFile(resolve('./src/index.html')))
 
 routes.get('/xml', XmlController.index)
 routes.get('/xmlToJson', XmlController.xmlToJson)
